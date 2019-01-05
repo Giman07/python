20c9414ded6e10ce595d051d45479f5e28568e5a
Giman07 <ghkswns92@gmail.com>
2019-01-05 22:23:27 +0900

    brA delete

--- login.py
+++ login.py
@@ -1,10 +1,7 @@
 import selenium
 from selenium import webdriver
-import re
-driver = webdriver.Chrome('C:\chromedriver.exe')
-driver.get('https://www.dogdrip.net/index.php?mid=dogdrip&page=2')
 driver.implicitly_wait(1)
-driver.find_element_by_xpath('//*[@id="navbar"]/div/div[3]/div[2]').click()
+driver.find_element_by_xpath('dd').click()
 elem=driver.find_element_by_xpath('//*[@id="id"]')
 elem.send_keys('ghkswns92')
 elem=driver.find_element_by_xpath('//*[@id="fo_member_login"]/fieldset/div[2]/input')
